2fef499b207a132bb6b4ee0a8ed53b90a03d1651
lufasoch <Lufasoch@gmail.com>
2016-05-24 22:19:48 -0300

    clase aun no creada
    Comente una línea que contiene un error

--- commons/src/com/usbus/commons/auxiliaryClasses/RouteStop.java
+++ commons/src/com/usbus/commons/auxiliaryClasses/RouteStop.java
@@ -7,5 +7,5 @@ public class RouteStop {
     private String busStop;
     private Double km;
     private boolean isCombinationPoint;
-    private Bus combinationBus;
+    //private Bus combinationBus; *ver* falta clase Bus 24/05/2016
 }
