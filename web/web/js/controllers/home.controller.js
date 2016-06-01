208c42bc61c086fdc35f22e3486b70222886c6c1
Lucia Salvarrey <lsalvarrey@altimetrik.com>
2016-06-01 11:29:50 -0300

    Styles

--- web/web/js/controllers/home.controller.js
+++ web/web/js/controllers/home.controller.js
@@ -5,15 +5,22 @@
 (function () {
     'use strict';
     angular.module('usbus').controller('HomeController', HomeController);
-    HomeController.$inject = ['$rootScope', 'localStorage'];
+    HomeController.$inject = ['$scope', '$rootScope', 'localStorage', '$location'];
     /* @ngInject */
-    function HomeController($rootScope, localStorage) {
-        $rootScope.show = typeof localStorage.getData('token') !== 'undefined' && localStorage.getData('token') != null;
-
+    function HomeController($scope, $rootScope, localStorage, $location) {
+		$rootScope.show = true;
+        /*$rootScope.show = typeof localStorage.getData('token') !== 'undefined' && localStorage.getData('token') != null;
+*/
         //TODO: la variable show depende de si el loquito esta dentro de un menu con muchas opciones o no
 
         if (typeof localStorage.getData('tenantName') !== 'undefined' && localStorage.getData('tenantName') != null) {
             $rootScope.tenantName = localStorage.getData('tenantName');
         }
+		
+		$scope.redirectTo = redirectTo;
+		
+		function redirectTo(path) {
+			$location.url($location.path() + '/' + path);
+		}
     }
 })();
--- web/web/js/usbus.routes.js
+++ web/web/js/usbus.routes.js
@@ -13,7 +13,31 @@
             })
             .when('/tenant/:tenantName', {
                 templateUrl: 'templates/home.html',
-                controller: 'LoginController'
+                controller: 'HomeController'
+            })
+			.when('/tenant/:tenantName/admin', {
+                templateUrl: 'templates/admin.html',
+                controller: 'HomeController'
+            })
+			.when('/tenant/:tenantName/tickets', {
+                templateUrl: 'templates/tickets.html',
+                controller: 'HomeController'
+            })
+			.when('/tenant/:tenantName/boxes', {
+                templateUrl: 'templates/boxes.html',
+                controller: 'HomeController'
+            })
+			.when('/tenant/:tenantName/workshop', {
+                templateUrl: 'templates/workshop.html',
+                controller: 'HomeController'
+            })
+			.when('/tenant/:tenantName/accountant', {
+                templateUrl: 'templates/accountant.html',
+                controller: 'HomeController'
+            })
+			.when('/tenant/:tenantName/humanResources', {
+                templateUrl: 'templates/humanResources.html',
+                controller: 'HomeController'
             })
             .otherwise({
                 redirectTo: '/'
