2ce0d20c1c2f7b47ff935f487059e436c29ea46c
Lucia Salvarrey <lsalvarrey@altimetrik.com>
2016-06-09 16:46:57 -0300

    bus y busstop lpm

--- web/web/js/controllers/bus.create.controller.js
+++ web/web/js/controllers/bus.create.controller.js
@@ -4,9 +4,9 @@
 (function () {
     'use strict';
     angular.module('usbus').controller('CreateBusController', CreateBusController);
-    CreateBusController.$inject = ['$scope', 'localStorage', 'BusResource'];
+    CreateBusController.$inject = ['$scope', 'localStorage', 'BusResource', '$mdDialog'];
     /* @ngInject */
-    function CreateBusController($scope, localStorage, BusResource) {
+    function CreateBusController($scope, localStorage, BusResource, $mdDialog) {
         $scope.createBus = createBus;
         $scope.cancel = cancel;
         $scope.showAlert = showAlert;
@@ -16,7 +16,19 @@
         }
 
         function createBus(bus) {
-            BusResource.save(bus,function (resp) {
+            bus.status = 'ACTIVE';
+            bus.tenantId = $scope.tenantId;
+            var token = null;//localStorage.getData('token');
+            if (localStorage.getData('token') != null && localStorage.getData('token') != '') {
+                token = localStorage.getData('token');
+            }
+
+            console.log(bus);
+
+            BusResource.buses(token).save(bus,{
+                tenantId: $scope.tenantId
+            },function (resp) {
+                console.log(resp);
                 showAlert('Exito!', 'Se ha creado su unidad de forma exitosa');
             }, function (error) {
                 console.log(error);
