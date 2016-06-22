9fce0e31479e2dfd14c1403825807de0f28977a3
lufasoch <Lufasoch@gmail.com>
2016-06-22 20:51:20 -0300

    Branch Services
    Branch Services

---
+++ bll/src/com/usbus/bll/administration/interfaces/BranchRemote.java
@@ -0,0 +1,18 @@
+package com.usbus.bll.administration.interfaces;
+
+import com.usbus.dal.model.Branch;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+/**
+ * Created by Lufasoch on 22/06/2016.
+ */
+public interface BranchRemote {
+    ObjectId persist(Branch branch);
+    Branch getById(ObjectId branchId);
+    Branch getByLocalId(long tenantId, Long branchId);
+    Branch getByBranchName(long tenantId, String name);
+    void setInactive(long tenantId, String branchName);
+    List<Branch> getBranchesByTenant(long tenantId, int offset, int limit);
+}
