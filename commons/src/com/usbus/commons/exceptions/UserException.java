6c5289da0e37d12c6163e3f0758a0288aaf6754e
Juan Pablo Martínez Cabrera <jpmc21891@gmail.com>
2016-06-26 16:59:05 -0300

    User Service

---
+++ commons/src/com/usbus/commons/exceptions/UserException.java
@@ -0,0 +1,11 @@
+package com.usbus.commons.exceptions;
+
+/**
+ * Created by jpmartinez on 26/06/16.
+ */
+public class UserException extends Exception{
+    public UserException(String mensaje) {
+        super(mensaje);
+    }
+}
+
