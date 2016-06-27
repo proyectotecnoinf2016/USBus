f29b488567b1754a9dd635b00f90391b65160320
Lucia Salvarrey <lusalvarrey@gmail.com>
2016-06-27 16:51:57 -0300

    file upload

--- web/web/js/controllers/tenant.styles.controller.js
+++ web/web/js/controllers/tenant.styles.controller.js
@@ -4,9 +4,23 @@
 (function () {
     'use strict';
     angular.module('usbus').controller('TenantController', TenantController);
-    TenantController.$inject = [];
+    TenantController.$inject = ['$scope'];
     /* @ngInject */
-    function TenantController() {
+    function TenantController($scope) {
+    	$scope.upload = upload;
 
-    }
+    	function upload(file) {
+			console.log (file);
+		    if (file) {
+		        // create an object for the ids
+			      var pictureId;
+
+			      // create a new formdata to store our image
+			      var fd = new FormData();
+			      fd.append('photo', file);
+			      
+			      console.log (fd);
+		    }
+	    }
+	}
 })();
