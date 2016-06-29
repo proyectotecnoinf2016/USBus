e311df890df4be32ed5f161f643931af78cd1553
lufasoch <Lufasoch@gmail.com>
2016-06-28 23:08:43 -0300

    Maintenance Services
    Get by Bus and between dates

--- bll/src/com/usbus/bll/administration/interfaces/MaintenanceRemote.java
+++ bll/src/com/usbus/bll/administration/interfaces/MaintenanceRemote.java
@@ -3,6 +3,7 @@ package com.usbus.bll.administration.interfaces;
 import com.usbus.dal.model.Maintenance;
 
 import javax.ejb.Remote;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -15,4 +16,7 @@ public interface MaintenanceRemote {
     Maintenance getById(String id);
     Maintenance getByLocalId(long tenantId, Long id);
     List<Maintenance> getByTenant(long tenantId, int offset, int limit);
+    List<Maintenance> getMaintenancesBetweenDates(long tenantId, Date time1, Date time2, int offset, int limit);
+    List<Maintenance> getByBus(long tenantId, String busId, int offset, int limit);
+
 }
