db8c23b33d24809f631ddd4f0db8ed863bc21ed0
Lucia Salvarrey <lsalvarrey@altimetrik.com>
2016-07-03 21:37:22 -0300

    bus navigate arreglado

--- web/web/js/controllers/journey.create.controller.js
+++ web/web/js/controllers/journey.create.controller.js
@@ -3,48 +3,74 @@
  */
 (function () {
     'use strict';
-    angular.module('usbus').controller('CreateBranchController', CreateBranchController);
-    CreateBranchController.$inject = ['$scope', 'localStorage', 'BranchResource', '$mdDialog', 'theme'];
+    angular.module('usbus').controller('CreateJourneyController', CreateJourneyController);
+    CreateJourneyController.$inject = ['$scope', 'localStorage', 'JourneyResource', 'ServiceResource', 'BusResource', '$mdDialog', 'theme'];
     /* @ngInject */
-    function CreateBranchController($scope, localStorage, BranchResource, $mdDialog, theme) {
-        $scope.createBranch = createBranch;
+    function CreateJourneyController($scope, localStorage, JourneyResource, ServiceResource, BusResource,  $mdDialog, theme, date) {
         $scope.cancel = cancel;
         $scope.showAlert = showAlert;
-        $scope.addWindow = addWindow;
-        $scope.deleteWindow = deleteWindow;
+        $scope.createJourney = createJourney;
+        $scope.queryService = queryService;
+        $scope.queryBus = queryBus;
 
         $scope.theme = theme;
-        $scope.branch = [];
-        $scope.windows = [];
+        $scope.date = date;
 
+        $scope.tenantId = 0;
         if (typeof localStorage.getData('tenantId') !== 'undefined' && localStorage.getData('tenantId') != null) {
             $scope.tenantId = localStorage.getData('tenantId');
         }
 
-        function createBranch(branch) {
-            branch.windows = $scope.windows;
-            BranchResource.save(branch,function (resp) {
+        var token = null;//localStorage.getData('token');
+        if (localStorage.getData('token') != null && localStorage.getData('token') != '') {
+            token = localStorage.getData('token');
+        }
+
+        function queryService(name) {
+            $scope.dayOfWeek = moment($scope.date).format('dddd').toUpperCase();
+            console.log($scope.dayOfWeek);
+            var lista = ServiceResource.services(token).query({
+                offset: 0,
+                limit: 100,
+                tenantId: $scope.tenantId,
+                query: 'DAYOFWEEK',
+                dayOfWeek: $scope.dayOfWeek
+
+            }).$promise;
+            console.log(lista);
+
+            return lista;
+        }
+
+
+        function queryBus(busName) {
+
+            return BusResource.buses(token).query({
+                query:"ALL",
+                status:true,
+                offset: 0,
+                limit: 5,
+                tenantId: $scope.tenantId
+            }).$promise;
+            return [];
+        }
+
+
+        function createJourney(item) {
+            item.tenantId = $scope.tenantId;
+            item.date = moment($scope.date).format('YYYY-MM-DDTHH:mm:ss.000Z');
+            item.service = $scope.service.id;
+            JourneyResource.save(item, function (resp) {
+                console.log(resp);
                 showAlert('Exito!', 'Se ha creado su unidad de forma exitosa');
             }, function (error) {
                 console.log(error);
                 showAlert('Error!', 'Ocurrió un error al registrar el TENANT');
-            } );
+            });
         }
 
-        function addWindow() {
-            $scope.windows.push({index: $scope.windows.length + 1,tickets : false, parcels : false});
-            console.log($scope.windows);
-        }
 
-        function deleteWindow(index) {
-            $scope.windows.splice(index, 1);
-            var i = 0;
-            for (i = 0; i < $scope.windows.length; i++) {
-                $scope.windows[i].index = i + 1;
-            }
-        }
-
-        function showAlert(title,content) {
+        function showAlert(title, content) {
             $mdDialog
                 .show($mdDialog
                     .alert()
