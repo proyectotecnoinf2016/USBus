ec1d8163114bec16895ead1986df5057616905a3
Lucia Salvarrey <lsalvarrey@altimetrik.com>
2016-07-05 23:20:18 -0300

    journey html

--- web/web/js/controllers/journey.selection.controller.js
+++ web/web/js/controllers/journey.selection.controller.js
@@ -4,20 +4,37 @@
 (function () {
     'use strict';
     angular.module('usbus').controller('TicketsController', TicketsController);
-    TicketsController.$inject = ['$scope', '$mdDialog', 'JourneyResource', 'localStorage', '$rootScope', '$location', 'dayOfWeek'];
+    TicketsController.$inject = ['$scope', '$mdDialog', 'JourneyResource', 'localStorage', '$rootScope', '$location', 'dayOfWeek', 'TicketResource'];
     /* @ngInject */
-    function TicketsController($scope, $mdDialog, JourneyResource, localStorage, $rootScope, $location, dayOfWeek ) {
+    function TicketsController($scope, $mdDialog, JourneyResource, localStorage, $rootScope, $location, dayOfWeek, TicketResource ) {
+        //FUNCTIONS
+        $scope.selectedSeat = selectedSeat;
+        $scope.exists = exists;
+        $scope.soldSeat = soldSeat;
+        $scope.cancel = cancel;
+        $scope.nextTab = nextTab;
+        $scope.sell = sell;
         $scope.showTicket = showTicket;
         $scope.getJourneys = getJourneys;
 
         $scope.tenantId = 0;
+        $scope.journeyNotSelected = true;
 
+        //SEATS VARIABLES
+        $scope.selected = [];
+        $scope.seats = [];
+        $scope.firstRow = [];
+        $scope.secondRow = [];
+        $scope.thirdRow = [];
+        $scope.fourthRow = [];
+        $scope.soldSeats = [];
 
 
         $scope.from = '';
         $scope.to = '';
         $scope.date = '';
         $scope.showJourneys = true;
+
 
         $rootScope.$emit('options', 'tickets');
 
@@ -61,23 +78,139 @@
 
 
 
-        function showTicket(item, ev) {
+        function showTicket(journey, ev) {
+            $scope.journeyNotSelected = false;
+            console.log(journey);
+            nextTab();
             $scope.showJourneys = false;
-            /*$mdDialog.show({
-                controller : 'CreateTicketController',
-                templateUrl : 'templates/ticket.create.html',
-                locals:{journey: item}, 
-                parent : angular.element(document.body),
-                targetEvent : ev,
-                clickOutsideToClose : false
-            }).then(
-                function(answer) {
-                    $scope.status = 'You said the information was "'
-                        + answer + '".';
-                }, function() {
-                    $scope.status = 'You cancelled the dialog.';
-                });*/
+            $scope.journey = journey;
+            if (journey.seatsState != null) {
+                var i = 0;
+                for (i = 0; i < journey.seatsState.length; i++) {
+                    $scope.soldSeats.push(journey.seatsState[i].number);
+                }
+            }
+            else {
+                journey.seatsState = [];
+            }
+
+            var i = 1;
+
+            var seatsCount = journey.seats;
+
+            while (i <= seatsCount) {
+                if (i <= seatsCount) {
+                    $scope.firstRow.push(i);
+                    i++;
+                }
+                if (i <= seatsCount) {
+                    $scope.secondRow.push(i);
+                    i++;
+                }
+                if (i <= seatsCount) {
+                    $scope.thirdRow.push(i);
+                    i++;
+                }
+                if (i <= seatsCount) {
+                    $scope.fourthRow.push(i);
+                    i++;
+                }
+            }
+
         };
+
+        function selectedSeat(item) {
+            var i = 0;
+            var exists = false;
+            var existsIndex = 0;
+
+            while (i < $scope.selected.length) {
+                if ($scope.selected[i] == item) {
+                    exists = true;
+                    existsIndex = i;
+                }
+                i++;
+            }
+
+            if (exists) {
+                $scope.selected.splice(existsIndex, 1);
+            }
+            else {
+                $scope.selected.push(item);
+            }
+        }
+
+        function soldSeat(item, list) {
+            return list.indexOf(item) > -1;
+        }
+
+        function sell() {
+            var journey = $scope.journey;
+            var ticket = [];
+            console.log($scope.journey);
+            $scope.userName = 0;
+            if (typeof localStorage.getData('userName') !== 'undefined' && localStorage.getData('userName') != null) {
+                $scope.userName = localStorage.getData('userName');
+            }
+
+            var token = null;//localStorage.getData('token');
+            if (localStorage.getData('token') != null && localStorage.getData('token') != '') {
+                token = localStorage.getData('token');
+            }
+
+            ticket.tenantId = $scope.tenantId;
+            ticket.amount = 0;
+            ticket.passengerName = '';
+            ticket.sellerName = $scope.userName;
+            ticket.closed = true;
+            ticket.status = 'CONFIRMED';
+            ticket.journeyId = $scope.journey.Id;
+            ticket.seat = $scope.selected[0];
+            ticket.getOnStopName = $scope.getOnStopName;
+            ticket.getOffStopName = $scope.getOffStopName;
+
+            ticket.emissionDate = new Date();//2016-07-06T01:50:45.077Z
+            console.log('ticket');
+            console.log(ticket);
+            TicketResource.tickets(token).save({
+                tenantId: $scope.tenantId
+
+            }, ticket,function (resp) {
+                console.log(resp);
+                alert('bien ahi');
+            }, function (error) {
+                console.log(error);
+            } );
+
+
+
+            /*
+
+             private Double amount;
+             private String passengerName;
+             private String sellerName;
+             private Boolean closed;
+             private TicketStatus status;
+             private Long journeyId;
+             private Integer seat;
+             private String getOnStopName;
+             private String getOffStopName;
+
+             */
+        }
+
+        function exists(item, list) {
+            return list.indexOf(item) > -1;
+        };
+
+        function cancel() {
+            $scope.selectedIndex = 0;
+        };
+
+        function nextTab() {
+            var index = ($scope.selectedIndex == $scope.max) ? 0 : $scope.selectedIndex + 1;
+            $scope.selectedIndex = index;
+        }
 
     }
 })();
